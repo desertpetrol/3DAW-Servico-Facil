9761aceb21ab739290fb88be46841ad1fb012928
Pedro <pedroaguiar254@gmail.com>
2024-12-02 00:18:22 -0300

    test

---
+++ pages/servicos.php
@@ -0,0 +1,13 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'] . '/pages/common/common.php';
+head();
+navbar();
+?>
+
+<h1>Nada aqui ainda :) </h1>
+
+<script src="script/services.js"></script>
+
+<?php
+foot();
+?>

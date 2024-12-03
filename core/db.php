ebae866bc047ff7f3a6a4d025c274302a458e77e
dsertpetrol <pedroaguiar2548@gmail.com>
2024-12-03 16:44:49 -0300

    singlogin

--- core/db.php
+++ core/db.php
@@ -2,10 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-
+$database = "db";
 
 try {
-  $pdo = new PDO("mysql:host=$servername;dbname=dados", $username, $password);
+  $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
   // set the PDO error mode to exception
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
